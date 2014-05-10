5a213ef2da7ea0723e8d02349a1a1f5209c6223c
Alejandro Machado <alemacgo@gmail.com>
2014-05-10 10:18:42 +0100

    Fix laggy changes to the users by using viewWillAppear

--- buildUP/FriendsTableViewController.m
+++ buildUP/FriendsTableViewController.m
@@ -18,6 +18,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
     self.friendsRelation = [[PFUser currentUser] objectForKey:@"friendsRelation"];
     PFQuery *query = [self.friendsRelation query];
     [query orderByAscending:@"username"];
